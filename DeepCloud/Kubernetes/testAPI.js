3d24efc0e381f72ef979281e3319c4969c4c756d
rashmitiwari <trashmi12@gmail.com>
2016-12-15 22:17:01 -0500

    Rashmi: December 15, 2016

--- DeepCloud/Kubernetes/testAPI.js
+++ DeepCloud/Kubernetes/testAPI.js
@@ -1,2 +1,2 @@
 var x = require('./DockerBackendInterface.js') 
-x.deploy_application("20");
+x.deploy_application("{\"width\":\"28\",\"height\":\"28\",\"nClass\":\"10\",\"alpha\":\"0.01\",\"File Name\":\"MNIST_data\",\"modelID\":\"testID\"}");
